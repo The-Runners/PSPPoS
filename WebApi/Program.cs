9d1a3dbd28286841b8c1b0c3e1a8aefa92c659b7
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2023-12-17 16:04:30 +0200

    fix launch url, add endpoints, add swagger

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,13 +1,14 @@
 using Infrastructure;
+using Microsoft.AspNetCore.OpenApi;
 using Microsoft.EntityFrameworkCore;
-using System.Text.Json.Serialization;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
-builder.Services.ConfigureHttpJsonOptions(options =>
-{
-    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-});
+//builder.Services.ConfigureHttpJsonOptions(options =>
+//{
+
+//});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
@@ -15,29 +16,184 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString);
 });
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "PSPPoS API",
+        Description = "An ASP.NET Core Web API for a PoS system.",
+    });
+});
+
 var app = builder.Build();
 
-var sampleTodos = new Todo[] {
-    new(1, "Walk the dog"),
-    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-    new(4, "Clean the bathroom"),
-    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-};
+app.UseSwagger();
+app.UseSwaggerUI();
 
-var todosApi = app.MapGroup("/todos");
-todosApi.MapGet("/", () => sampleTodos);
-todosApi.MapGet("/{id}", (int id) =>
-    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-        ? Results.Ok(todo)
-        : Results.NotFound());
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
+var customerApi = app.MapGroup("/customer").WithTags("Customers").WithOpenApi();
+
+customerApi.MapGet("/{id}", (Guid id) =>
+{
+    throw new NotImplementedException();
+});
+
+customerApi.MapPut("/{id}", (Guid id) =>
+{
+    throw new NotImplementedException();
+});
+
+customerApi.MapPost("/", () =>
+{
+    throw new NotImplementedException();
+});
+
+customerApi.MapDelete("/{id}", (Guid id) =>
+{
+    throw new NotImplementedException();
+});
+
+var ordersApi = app.MapGroup("/order").WithTags("Orders").WithOpenApi();
+
+ordersApi.MapGet("/", () =>
+{
+    throw new NotImplementedException();
+});
+
+ordersApi.MapPost("/", () =>
+{
+    throw new NotImplementedException();
+});
+
+ordersApi.MapPut("/{id}", (Guid id) =>
+{
+    throw new NotImplementedException();
+});
+
+ordersApi.MapGet("/{id}", (Guid id) =>
+{
+    throw new NotImplementedException();
+});
+
+ordersApi.MapDelete("/{id}", (Guid id) =>
+{
+    throw new NotImplementedException();
+});
+
+var paymentApi = app.MapGroup("/payment").WithTags("Payments");
+
+paymentApi.MapPost("/", () =>
+{
+    throw new NotImplementedException();
+});
+
+paymentApi.MapGet("/validate", () =>
+{
+    throw new NotImplementedException();
+});
+
+var reservationApi = app.MapGroup("/reservation").WithTags("Reservations");
+
+reservationApi.MapGet("/", () =>
+{
+    throw new NotImplementedException();
+});
+
+reservationApi.MapPost("/", () =>
+{
+    throw new NotImplementedException();
+});
+
+reservationApi.MapPut("/{id}", (Guid id) =>
+{
+    throw new NotImplementedException();
+});
+
+reservationApi.MapGet("/{id}", (Guid id) =>
+{
+    throw new NotImplementedException();
+});
+
+reservationApi.MapDelete("/{id}", (Guid id) =>
+{
+    throw new NotImplementedException();
+});
+
+var inventoryApi = app.MapGroup("/inventory").WithTags("Inventory");
+
+inventoryApi.MapGet("/{itemId}/availability", (Guid itemId) =>
+{
+    throw new NotImplementedException();
+});
+
+inventoryApi.MapPut("/{itemId}", (Guid itemId) =>
+{
+    throw new NotImplementedException();
+});
+
+var employeeApi = app.MapGroup("/employee").WithTags("Employees");
+
+employeeApi.MapGet("/", () =>
+{
+    throw new NotImplementedException();
+});
+
+employeeApi.MapPost("/{id}/loghours", (Guid id) =>
+{
+    throw new NotImplementedException();
+});
+
+employeeApi.MapGet("/{id}/schedule", (Guid id) =>
+{
+    throw new NotImplementedException();
+});
+
+employeeApi.MapPut("/{id}/schedule", (Guid id) =>
+{
+    throw new NotImplementedException();
+});
+
+var loyaltyApi = app.MapGroup("/loyalty").WithTags("Loyalty program");
+
+loyaltyApi.MapPost("/enroll", () =>
+{
+    throw new NotImplementedException();
+});
+
+loyaltyApi.MapPost("/{id}/applyBenefits", (Guid id) =>
+{
+    throw new NotImplementedException();
+});
+
+var marketingApi = app.MapGroup("/marketing").WithTags("Marketing");
+
+marketingApi.MapPost("/discounts/apply", () =>
+{
+    throw new NotImplementedException();
+});
+
+marketingApi.MapPost("/promotions", (Guid id) =>
+{
+    throw new NotImplementedException();
+});
+
+var notificationsApi = app.MapGroup("/notifications").WithTags("Notifications"); ;
+
+notificationsApi.MapPost("/{customerId}/send", (Guid customerId) =>
+{
+    throw new NotImplementedException();
+});
+
+notificationsApi.MapGet("/{customerId}", (Guid customerId) =>
+{
+    throw new NotImplementedException();
+});
 
 app.Run();
-
-public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
-
-[JsonSerializable(typeof(Todo[]))]
-internal partial class AppJsonSerializerContext : JsonSerializerContext
-{
-
-}
