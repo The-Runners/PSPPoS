306682497c284cb9f011fcc8a8b7cc2c475fa333
Lukas <luk.brasiunas@gmail.com>
2024-01-09 20:45:54 +0200

    Refactored the ProductService Edit method

--- Domain/Models/Product.cs
+++ Domain/Models/Product.cs
@@ -2,9 +2,9 @@
 
 public class Product
 {
-    public required Guid Id { get; init; }
+    public required Guid Id { get; set; }
 
-    public required string Name { get; init; }
+    public required string Name { get; set; }
 
-    public required decimal Price { get; init; }
+    public required decimal Price { get; set; }
 }
